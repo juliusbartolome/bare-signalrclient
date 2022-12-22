8649d0858b32355c1732d7c6e79a580bcb98b00b
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2022-12-22 13:04:31 +0800

    added bare signalr client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import signalR from '@microsoft/signalr';
 
 function App() {
+  const url = "";
+  const subscribionMethodName = "";
+  const subscriptionArgs: any[] = [];
+  const subscribeToMethodName = "";
+
+  let connection = new signalR.HubConnectionBuilder()
+    .withUrl(url)
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+  connection.on(subscribeToMethodName, data => {
+      console.log(`${subscribeToMethodName} executed`);
+      console.log(data);
+  });
+
+  connection.start()
+      .then(() => connection.invoke(subscribionMethodName, subscriptionArgs));
+
   return (
     <div className="App">
       <header className="App-header">
